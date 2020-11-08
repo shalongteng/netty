27b5ef2aaeca0ce063a4cd72adbf04361de9b20f
shalongteng <1131790681@qq.com>
2020-11-08 23:35:10 +0800

    udp

--- src/main/java/com/slt/netty/socket/UdpClient.java
+++ src/main/java/com/slt/netty/socket/UdpClient.java
@@ -1,6 +1,8 @@
 package com.slt.netty.socket;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
@@ -10,7 +12,12 @@ import java.net.InetSocketAddress;
  */
 public class UdpClient {
     public static void main(String[] args) throws Exception {
-        byte[] buf = new String("hello").getBytes();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
+        //将long类型值800 写到了字节数组里
+        dataOutputStream.writeLong(800);
+
+        byte[] buf = byteArrayOutputStream.toByteArray();
         //把字节数组 扔到包袱里
         DatagramPacket datagramPacket = new DatagramPacket(buf, buf.length,new InetSocketAddress("127.0.0.1",6666));
 
--- src/main/java/com/slt/netty/socket/UdpServer.java
+++ src/main/java/com/slt/netty/socket/UdpServer.java
@@ -1,5 +1,9 @@
 package com.slt.netty.socket;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
@@ -9,15 +13,17 @@ import java.net.SocketException;
  */
 public class UdpServer {
     public static void main(String[] args) throws Exception {
-        byte[] buf   = new byte[1024];
+        byte[] buf = new byte[1024];
         //包裹 用来接收对方发过来的数据
         DatagramPacket datagramPacket = new DatagramPacket(buf, buf.length);
         DatagramSocket datagramSocket = new DatagramSocket(6666);
-
         while (true){
+            //使用 包袱 初始化 字节数组流
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(buf);
+            DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
             //接收数据 扔到包袱中去   阻塞等待
             datagramSocket.receive(datagramPacket);
-            System.out.println(new String(buf,0,datagramPacket.getLength()));
+            System.out.println(dataInputStream.readLong());
         }
     }
 }
