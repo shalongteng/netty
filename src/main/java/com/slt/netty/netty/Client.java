6234e6cad64824fbe420fb6e89f88e3633a744b4
yuyanan <yuyanan@handongkeji.com>
2020-11-11 18:22:58 +0800

    netty client

--- src/main/java/com/slt/netty/netty/Client.java
+++ src/main/java/com/slt/netty/netty/Client.java
@@ -9,31 +9,52 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.ReferenceCountUtil;
 
+/**
+ * netty 基于事件模型
+ * netty里所有的方法都是异步的
+ */
 public class Client {
     public static void main(String[] args) {
         new Client().clientStart();
     }
 
     private void clientStart() {
-        EventLoopGroup workers = new NioEventLoopGroup();
+        //事件处理的线程池  connect read write
+        EventLoopGroup workers = new NioEventLoopGroup(5);
+        //辅助启动
         Bootstrap b = new Bootstrap();
         b.group(workers)
                 .channel(NioSocketChannel.class)
+                //channel 的初始化器
                 .handler(new ChannelInitializer<SocketChannel>() {
-
+                    //channel 初始化的时候调用initChannel，此时还没有连接上
                     @Override
-                    protected void initChannel(SocketChannel ch) throws Exception {
+                    protected void initChannel(SocketChannel channel) throws Exception {
                         System.out.println("channel initialized!");
-                        ch.pipeline().addLast(new ClientHandler());
+                        //observer 套 observer
+                        channel.pipeline().addLast(new ClientHandler());
                     }
                 });
 
         try {
             System.out.println("start to connect...");
-            ChannelFuture f = b.connect("127.0.0.1", 8888).sync();
+            //connect 异步方法如果不调用sync方法，需要加一个监听器来回调  sync同步阻塞等待
+            ChannelFuture future = b.connect("127.0.0.1", 8888);
 
-            f.channel().closeFuture().sync();
-
+            //异步的写法 加一个监听器listener
+            future.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if(future.isSuccess()){
+                        System.out.println("netty connected");
+                    }else {
+                        System.out.println("netty not connected");
+                    }
+                }
+            });
+            //不写同步等待，看不到异常的抛出
+            future.sync();
+            future.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
 
