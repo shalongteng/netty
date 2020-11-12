d1cbd3738c1aee17649555afe10a1d85493aeb27
shalongteng <1131790681@qq.com>
2020-11-12 21:51:49 +0800

    netty实现群发

--- src/main/java/com/slt/netty/netty/HelloNetty.java
+++ src/main/java/com/slt/netty/netty/HelloNetty.java
@@ -3,11 +3,14 @@ package com.slt.netty.netty;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 /**
  * sync方法保证上一个方法变成同步方法。
@@ -20,6 +23,7 @@ public class HelloNetty {
 
 class NettyServer {
     int port = 8888;
+    public static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
     public NettyServer(int port) {
         this.port = port;
@@ -70,18 +74,27 @@ class NettyServer {
 }
 
 class Handler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        NettyServer.channelGroup.add(ctx.channel());
+    }
     /**
      * msg 数据已经读进来了
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         //super.channelRead(ctx, msg);
-        ByteBuf buf = (ByteBuf) msg;
-        System.out.println("server: channel read：" + buf.toString(CharsetUtil.UTF_8));
-        //写会给客户端
-        ctx.writeAndFlush(msg);
 
-        ctx.close();
+        //实现群发
+        NettyServer.channelGroup.writeAndFlush(msg);
+
+
+//        ByteBuf buf = (ByteBuf) msg;
+//        System.out.println("server: channel read：" + buf.toString(CharsetUtil.UTF_8));
+//        //写会给客户端
+//        ctx.writeAndFlush(msg);
+//
+//        ctx.close();
 
 //        ReferenceCountUtil.release(buf);
 //        buf.release();
